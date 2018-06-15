ed4cfadc052aa350cab20246de276c1cfc2248cb
Hunter Long <info@socialeck.com>
2018-06-14 21:19:09 -0700

    new

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"bytes"
 	"os"
+	"strings"
 )
 
 var (
@@ -37,45 +38,63 @@ func main() {
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.Handle("/", http.HandlerFunc(IndexHandler))
-	r.Handle("/{coin}/{address}/{type}", http.HandlerFunc(BadgeHandler))
-	r.Handle("/{coin}/{address}/{type}/usd", http.HandlerFunc(BadgeHandler))
+	r.Handle("/{coin}/{address}", http.HandlerFunc(NormalBadgeHandler))
 	return r
 }
 
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	badge := []byte(`<svg xmlns="http://www.w3.org/2000/svg" width="104" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="104" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h54v20H0z"/><path fill="#4c1" d="M54 0h50v20H54z"/><path fill="url(#b)" d="M0 0h104v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="28" y="15" fill="#010101" fill-opacity=".3">h7BowIw</text><text x="28" y="14">h7BowIw</text><text x="78" y="15" fill="#010101" fill-opacity=".3">12.533</text><text x="78" y="14">12.533</text></g></svg>`)
+	badge := []byte(`<svg xmlns="http://www.w3.org/2000/svg" width="1" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="120" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h80v20H0z"/><path fill="#4c1" d="M54 0h80v20H54z"/><path fill="url(#b)" d="M0 0h130v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="28" y="15" fill="#010101" fill-opacity=".3">h7BowIw</text><text x="28" y="14">h7BowIw</text><text x="78" y="15" fill="#010101" fill-opacity=".3">12.533</text><text x="78" y="14">12.533</text></g></svg>`)
 	w.Header().Set("Content-Type", "image/svg+xml")
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	w.Write(badge)
 }
 
-func BadgeHandler(w http.ResponseWriter, r *http.Request) {
+
+func CryptoBalance(coin, address string) float64 {
+	var balance float64
+	switch coin {
+	case "btc": balance = BitcoinBalance(address)
+	}
+	if balance == 0 {
+		balance = 0.0
+	}
+	return balance
+}
+
+type Badge struct {
+	Coin string
+	Address string
+	Type string
+}
+
+func NormalBadgeHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	coin, _ := vars["coin"]
 	address, _ := vars["address"]
 	badgeType, _ := vars["type"]
 
-	var balance float64
-
-	switch coin {
-		case "btc": balance = BitcoinBalance(address)
-	}
-	if balance == 0 {
-		balance = 0.0
+	badge := &Badge{
+		coin,
+		address,
+		badgeType,
 	}
 
-	fmt.Println("balance: ", balance)
+	fmt.Println(badge)
+
+	balance := CryptoBalance(coin, address)
 
 	fmt.Println(badgeType, coin)
 
-	addressFormat := address[0:6]
+	addressFormat := address[0:7]
 
-	badgeData := fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" width="99" height="20"><linearGradient id="b" x2="0" y2="100%%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="99" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h54v20H0z"/><path fill="#e05d44" d="M54 0h45v20H54z"/><path fill="url(#b)" d="M0 0h99v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="28" y="15" fill="#010101" fill-opacity=".3">%v</text><text x="28" y="14">%v</text><text x="75.5" y="15" fill="#010101" fill-opacity=".3">%0.3f %v</text><text x="75.5" y="14">%0.3f %v</text></g></svg>`, addressFormat, addressFormat, balance, coin, balance, coin)
+	coin = strings.ToUpper(coin)
 
-	badge := []byte(badgeData)
+	badgeData := fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="134" height="20"><linearGradient id="b" x2="0" y2="100%%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="134" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h59v20H0z"/><path fill="#97CA00" d="M59 0h75v20H59z"/><path fill="url(#b)" d="M0 0h134v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"><text x="305" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="490">%v</text><text x="305" y="140" transform="scale(.1)" textLength="490">%v</text><text x="955" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="650">%0.3f %v</text><text x="955" y="140" transform="scale(.1)" textLength="650">%0.3f %v</text></g> </svg>`, addressFormat, addressFormat, balance, coin, balance, coin)
 
-	WriteBadge(badge, w)
+	badgeD := []byte(badgeData)
+
+	WriteBadge(badgeD, w)
 }
 
 
